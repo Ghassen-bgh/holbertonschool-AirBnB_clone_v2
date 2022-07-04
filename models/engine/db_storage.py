8f7b05e95e4e9d6ad74f8a548efc27a077848c94
Ghassen-bgh <3591@holbertonschool.com>
2022-07-04 12:55:15 +0100

    db storage task 6

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,1 +1,79 @@
 #!/usr/bin/python3
+""" Storage sqlalchemy model """
+from os import getenv
+from models.base_model import BaseModel, Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+
+database = getenv("HBNB_MYSQL_DB")
+user = getenv("HBNB_MYSQL_USER")
+host = getenv("HBNB_MYSQL_HOST")
+password = getenv("HBNB_MYSQL_PWD")
+hbnb_env = getenv("HBNB_ENV")
+
+
+class DBStorage:
+    """class DBStorage"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """initialize instances"""
+        self.__engine = create_engine(
+            'mysql+mysqldb://{}:{}@{}/{}'
+            .format(user, password, host, database), pool_pre_ping=True)
+        if hbnb_env == "test":
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """ all  """
+        ls = []
+        if cls is not None:
+            if type(cls) is str:
+                cls = eval(cls)
+            ls = self.__session.query(cls).all()
+        else:
+            ls += self.__session.query(User).all()
+            ls += self.__session.query(State).all()
+            ls += self.__session.query(City).all()
+            ls += self.__session.query(Amenity).all()
+            ls += self.__session.query(Place).all()
+            ls += self.__session.query(Review).all()
+
+        new = {}
+        for x in ls:
+            tag = x.__class__.__name__ + "." + x.id
+            new[tag] = x
+        return new
+
+    def new(self, obj):
+        """ new  """
+        self.__session.add(obj)
+
+    def save(self):
+        """ save  """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """ delete  """
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """ reload  """
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
+    def close(self):
+        """close session"""
+        self.__session.close()
